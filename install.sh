6893f504f50d26b5fd227e4ed03415023ca73fac
kaftz <sn.setaris@gmail.com>
2015-05-09 03:36:39 -0700

    add install

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="vimrc tmux.conf"
+
+echo "creating $olddir to store existing files"
+mkdir -p $olddir
+
+for file in $files; do
+  mv ~/.$file $olddir
+  echo "copying $file"
+  ln -s $dir/$file ~/.$file
+done
